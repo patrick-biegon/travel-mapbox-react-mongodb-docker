5a50c15c68ab1687b1170a5cc15debf390ae4847
Asgar Ali <asgaralipq@gmail.com>
2021-02-22 17:22:56 +0530

    added markersfrom react map gl

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
 import {listLogEntries} from './api';
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
     latitude: 21.1458,
-    longitude: 79.0882,
+    longitude: 79.0882, 
     zoom: 3
   });
 
 useEffect(() => {
   (async () => {
     const logEntries = await listLogEntries();
+    setLogEntries(logEntries);
     console.log(logEntries);
 
   })();
@@ -27,7 +29,36 @@ useEffect(() => {
       mapStyle="mapbox://styles/asgaraliq/cklfm0y8e3zkx17o0la6s1hk6"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+      {
+        logEntries.map(entry =>(
+        <Marker 
+          key={entry._id}
+          latitude={entry.latitude} 
+          longitude={entry.longitude} 
+          offsetLeft={-12} 
+          offsetTop={-24}
+        >
+          <svg 
+          className="marker" 
+          style = {{
+            width:'24px',
+            height:'24px',
+          }}
+          viewBox="0 0 24 24" 
+          stroke-width="2" 
+          fill="none" 
+          stroke-linecap="round" 
+          stroke-linejoin="round"
+          >
+            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
+          </svg>
+        </Marker>
+
+        ))
+
+      }
+    </ReactMapGL>
   );
 }
 
