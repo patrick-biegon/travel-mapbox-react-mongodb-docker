e1645d3f5a7012a7bd56713b786fb3cf254040a1
Nimish Vithalani <nimish.v@somaiya.edu>
2021-03-11 15:07:43 +0530

    production

--- server/src/middlewares.js
+++ server/src/middlewares.js
@@ -9,7 +9,7 @@ const errorHandler = (error, req, res, next) => {
     res.status(statusCode);
     res.json({
         message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? 'LMAO 😂😂😂😂' : error.stack,
+        stack: process.env.NODE_ENV === 'production' ? 'LOL' : error.stack,
     });
 };
 
