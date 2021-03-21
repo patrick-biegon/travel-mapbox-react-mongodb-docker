4dc7cfc6a3c4d2698bac48d87f057c15c0b792d8
Nimish Vithalani <nimish.v@somaiya.edu>
2021-03-21 16:58:33 +0530

    end point for login created.

--- server/src/api/user.js
+++ server/src/api/user.js
@@ -14,6 +14,16 @@ router.post('/create', async (req, res) => {
         console.log(e)
         res.status(400).send(e);
     }
-}) 
+});
+
+router.post('/login', async(req, res) => {
+    try{
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.getAuthToken();
+        res.send({user, token});
+    }catch(e){
+        res.status(404).send(e);
+    }
+})
 
 module.exports = router;
