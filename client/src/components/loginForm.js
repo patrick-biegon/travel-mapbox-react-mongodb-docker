997d33b290d7eab78707b78feabb61aaeb120342
Nimish Vithalani <nimish.v@somaiya.edu>
2021-03-28 22:00:22 +0530

    state provider added to login form

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react"
 import { login } from "../api"
+import { useStateValue } from '../StateProvider';
+const { Redirect } = require("react-router-dom");
+
 
 const Login = ()=> {
+    var redirect = false;
     const [email, updateEmail] = useState("");
     const [password, updatePassword] = useState("");
+    const [state, updateState] = useState({
+        email: "",
+        password: "",
+        loading: false,
+        errors: {},
+        redirect: redirect,
+      });
 
     const handleUpdateEmail = (event) => {
         updateEmail(event.target.value)
@@ -15,12 +26,26 @@ const Login = ()=> {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        updateState({
+            loading: true,
+          });
         const status = await login(email, password)
         if(status){
             // Code you want to run after login is successful.
+            console.log(status);
+            updateState({
+                loading: false,
+                redirect: "../App",
+              });
+            
 
         } else {
             // Run if login failed.
+            console.log(status);
+            updateState({
+                errors: "Invalid Data",
+                loading: false,
+              });
         }
     }
 
