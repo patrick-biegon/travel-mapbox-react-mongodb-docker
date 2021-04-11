d87b0b928a1c43eb3a1166a7f69f89f70aab07d3
Nimish Vithalani <nimish.v@somaiya.edu>
2021-04-11 22:27:10 +0530

    router created for register

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
 import loginForm from './components/loginForm';
+import registerForm from './components/registerForm'
 import AboutUs from './components/AboutUs';
 import Help from './components/Help';
 import Logout from './components/Logout';
@@ -12,6 +13,7 @@ function Router1() {
                         
                         <Switch>
                                 <Route path="/" exact component={loginForm} />
+                                <Route path="/Register" exact component={registerForm} />
                                 <Route path="/App" exact component={App} />
                                 <Route path="/AboutUs" exact component={AboutUs} />
                                 <Route path="/Help" exact component={Help} />
