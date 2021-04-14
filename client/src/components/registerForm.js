5cb6bad7114b8205e57df0d763775455672b0522
Nimish Vithalani <nimish.v@somaiya.edu>
2021-04-14 23:59:35 +0530

    css changes

--- client/src/components/registerForm.js
+++ client/src/components/registerForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { register } from "../api";
-import {useStateValue} from '../StateProvider';
+import { useStateValue } from '../StateProvider';
 const { Redirect } = require("react-router-dom");
 
 const inputStyle = {
@@ -10,8 +10,8 @@ const inputStyle = {
     display: "inline-block",
     border: "1px solid #ccc",
     boxSizing: "border-box",
-  }
-  
+}
+
 const buttonStyle = {
     backgroundColor: "#4CAF50",
     color: "white",
@@ -20,21 +20,23 @@ const buttonStyle = {
     border: "none",
     cursor: "pointer",
     width: "100%",
-  }
+}
 
-const Register = ()=> {
+const Register = () => {
     var redirect = false;
     const [name, updateName] = useState("");
     const [email, updateEmail] = useState("");
     const [password, updatePassword] = useState("");
     const [state, updateState] = useState({
         name: "",
-        email:"",
-        password:"",
+        email: "",
+        password: "",
         loading: false,
         errors: {},
         redirect: redirect,
     });
+
+
 
     const handleUpdateName = (event) => {
         updateName(event.target.value)
@@ -47,6 +49,12 @@ const Register = ()=> {
     const handleUpdatePassword = (event) => {
         updatePassword(event.target.value)
     }
+    const handleBack = async (event) => {
+        updateState({
+            loading: false,
+            redirect: "/",
+        });
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -54,40 +62,43 @@ const Register = ()=> {
             loading: true,
         });
 
-        const status = await register(name, email, password)
-        if(status){
-            console.log(status);
-            updateState({
-                loading: false,
-                redirect: "/App",
-            });
-        } else {
-            console.log(status);
-            updateState({
-                errors: "Invalid Data",
-                loading: false,
-              });
+        
+
+            const status = await register(name, email, password)
+            if (status) {
+                console.log(status);
+                updateState({
+                    loading: false,
+                    redirect: "/App",
+                });
+            } else {
+                console.log(status);
+                updateState({
+                    errors: "Invalid Data",
+                    loading: false,
+                });
+            }
         }
-    }
 
-    if (state.redirect) {
-        return <Redirect to={state.redirect} />;
-      }
+        if (state.redirect) {
+            return <Redirect to={state.redirect} />;
+        }
 
-      if (localStorage.getItem("token")) {
-        return <Redirect to="/App" />;
-      }
+        if (localStorage.getItem("token")) {
+            return <Redirect to="/App" />;
+        }
 
 
-    return (
-        <div style ={{padding:'30%', paddingTop:'10%'}} >
-            <div><h2><center>Register</center></h2></div>
-            <input style={inputStyle} type="name" placeholder="Enter your Name" value={name} onChange={handleUpdateName}></input>
+        return (
+            <div style={{ padding: '30%', paddingTop: '10%' }} >
+                <div><h2><center>Register</center></h2></div>
+                <input style={inputStyle} type="name" placeholder="Enter your Name" value={name} onChange={handleUpdateName}></input>
                 <input style={inputStyle} type="email" placeholder="Enter your Email" value={email} onChange={handleUpdateEmail}></input>
                 <input style={inputStyle} type="password" placeholder="Enter password" value={password} onChange={handleUpdatePassword}></input>
                 <button style={buttonStyle} onClick={handleSubmit}>Register</button>
-        </div>
-    )
-}
+                <button style={buttonStyle} onClick={handleBack}>Back</button>
+            </div>
+        )
+    }
 
-export default Register
+    export default Register
