0a9f464520cd45f435974f1ce213e1477b8dd5bc
Asgar Ali <asgaralipq@gmail.com>
2021-04-17 00:23:13 +0530

    Test Changes

--- client/tests/test.js
+++ client/tests/test.js
@@ -36,127 +36,193 @@ describe("Home Page", function () {
         });
     });
 
-    it("Should check whether the navigation element is loaded", function () {
+    it("Should check whether the LoginForm element is loaded", function () {
         return new Promise((resolve, reject) => {
             browser
-                .findElement({ className: "nav" })
+                .findElement({ className: "LogInForm" })
                 .then(elem => resolve())
                 .catch(err => reject(err));
         });
     });
 
-    it("Should check whether the geolocation element is loaded", function () {
+    it("Should check whether the LogIn button element is loaded", function () {
         return new Promise((resolve, reject) => {
             browser
-                .findElement({ className: "geo" })
+                .findElement({ className: "Button1Style" })
                 .then(elem => resolve())
                 .catch(err => reject(err));
         });
     });
 
-    it("Should check whether the panel element is loaded", function () {
+    it("Should check whether the Create Account button is loaded", function () {
         return new Promise((resolve, reject) => {
             browser
-                .findElement({ className: "panel" })
+                .findElement({ className: "Button2Style" })
                 .then(elem => resolve())
                 .catch(err => reject(err));
         });
     });
 
-    it("Should check whether the theme element is loaded", function () {
+    it("Should check the Register Form is functional", function () {
         return new Promise((resolve, reject) => {
             browser
-                .findElement({ className: "themeToggle" })
-                .then(elem => resolve())
-                .catch(err => reject(err));
-        });
-    });
-
-    it("Should check the panel inputs", function () {
-        return new Promise((resolve, reject) => {
-            browser
-                .findElement({ className: "panel" })
-                .findElement({ className: "input" })
+                .findElement({ className: "LogInForm" })
+                .findElement({ className: "Button2Style" })
                 .click()
                 .then(elem => resolve())
                 .catch(err => reject(err));
         });
     });
 
-    it("Should check the geolocation button enable", function () {
+
+    it("Should check whether the Register Form is loaded or not", function () {
         return new Promise((resolve, reject) => {
             browser
-                .findElement({ className: "geo" })
-                .findElement({ className: "mapboxgl-ctrl-icon" })
-                .click()
+                .findElement({ className: "RegisterForm" })
                 .then(elem => resolve())
                 .catch(err => reject(err));
         });
     });
 
-    delay(1000);
-
-    it("Should check the geolocation button disable", function () {
+    it("Should check whether the Register button element is loaded", function () {
         return new Promise((resolve, reject) => {
             browser
-                .findElement({ className: "geo" })
-                .findElement({ className: "mapboxgl-ctrl-icon" })
-                .click()
+                .findElement({ className: "Button1Style" })
                 .then(elem => resolve())
                 .catch(err => reject(err));
         });
     });
 
-    delay(1000);
-
-    it("Should check the nav zoom in", function () {
+    it("Should check whether the Log In instead button is loaded", function () {
         return new Promise((resolve, reject) => {
             browser
-                .findElement({ className: "nav" })
-                .findElement({ className: "mapboxgl-ctrl-zoom-in" })
-                .click()
+                .findElement({ className: "Button2Style" })
                 .then(elem => resolve())
                 .catch(err => reject(err));
         });
     });
 
-    it("Should check the nav zoom out", function () {
-        return new Promise((resolve, reject) => {
-            browser
-                .findElement({ className: "nav" })
-                .findElement({ className: "mapboxgl-ctrl-zoom-out" })
-                .click()
-                .then(elem => resolve())
-                .catch(err => reject(err));
-        });
-    });
+    // it("Should check whether the navigation element is loaded", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "nav" })
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
 
-    it("Should check the themeToggle 1", function () {
-        return new Promise((resolve, reject) => {
-            browser
-                .findElement({ className: "themeToggle" })
-                .findElement({ className: "themeButton" })
-                .click()
-                .then(elem => resolve())
-                .catch(err => reject(err));
-        });
-    });
+    // it("Should check whether the geolocation element is loaded", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "geo" })
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
 
-    delay(1000);
+    // it("Should check whether the panel element is loaded", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "panel" })
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
 
-    it("Should check the themeToggle 2", function () {
+    // it("Should check whether the theme element is loaded", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "themeToggle" })
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
 
-        return new Promise((resolve, reject) => {
-            browser
-                .findElement({ className: "themeToggle" })
-                .findElement({ className: "themeButton" })
-                .click()
-                .then(elem => resolve())
-                .catch(err => reject(err));
-        });
-    });
+    // it("Should check the panel inputs", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "panel" })
+    //             .findElement({ className: "input" })
+    //             .click()
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
 
-    delay(1000);
+    // it("Should check the geolocation button enable", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "geo" })
+    //             .findElement({ className: "mapboxgl-ctrl-icon" })
+    //             .click()
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
+
+    // delay(1000);
+
+    // it("Should check the geolocation button disable", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "geo" })
+    //             .findElement({ className: "mapboxgl-ctrl-icon" })
+    //             .click()
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
+
+    // delay(1000);
+
+    // it("Should check the nav zoom in", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "nav" })
+    //             .findElement({ className: "mapboxgl-ctrl-zoom-in" })
+    //             .click()
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
+
+    // it("Should check the nav zoom out", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "nav" })
+    //             .findElement({ className: "mapboxgl-ctrl-zoom-out" })
+    //             .click()
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
+
+    // it("Should check the themeToggle 1", function () {
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "themeToggle" })
+    //             .findElement({ className: "themeButton" })
+    //             .click()
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
+
+    // delay(1000);
+
+    // it("Should check the themeToggle 2", function () {
+
+    //     return new Promise((resolve, reject) => {
+    //         browser
+    //             .findElement({ className: "themeToggle" })
+    //             .findElement({ className: "themeButton" })
+    //             .click()
+    //             .then(elem => resolve())
+    //             .catch(err => reject(err));
+    //     });
+    // });
+
+    // delay(1000);
 
     function delay(interval) {
         return it('should delay', done => {
